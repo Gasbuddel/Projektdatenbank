637adc530dd375b086cf6e8b67ff2d050f73eb83
Gasbuddel <ibrattka@web.de>
2013-05-31 19:20:41 +0200

    Sachmittel hinzugefuegt

---
+++ Projektdatenbank/Projektdatenbank/Models/TSachmittel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Projektdatenbank.Models
+{
+    public class TSachmittel
+    {
+        [Key]
+        public int SAId { get; set; }
+        [Required]
+        public string SABeschreibung { get; set; }
+        [Required]
+        public decimal SAKosten { get; set; }
+    }
+}
